00ccae6215229688dd5cb988e66fe8cbe0a2c449
nana72848e <d1041211005@student.untan.ac.id>
2023-12-31 19:22:05 +0700

    first  commit
    first commit

---
+++ frontend/saputipu/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/views/**/*.{vue,js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{vue,js,ts,jsx,tsx,mdx}',
+    './src/stores/**/*.{vue,js,ts,jsx,tsx,mdx}',
+    './src/router/**/*.{vue,js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
